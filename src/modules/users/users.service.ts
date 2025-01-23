6b42ad734e79545a59d4bbcf2c4fdd2e18a5347e
hugolfsimoes <hugolfsimoes@gmail.com>
2025-01-23 20:51:49 -0300

    (main): finish create user and user reposjtory

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,47 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcryptjs';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepo: UsersRepository) {}
+  async create(createUserDto: CreateUserDto) {
+
+    const { email, name, password } = createUserDto;
+
+    const emailTaken = await this.usersRepo.findUnique({
+      where: {
+        email
+      }, select: { id: true }
+    });
+
+    if (emailTaken) throw new ConflictException('Email is already in use');
+
+    const hashedPassword = await hash(password, 12);
+    const user = await this.usersRepo.create({
+      data: {
+        email, name, password: hashedPassword, categories: {
+          createMany: {
+            data: [
+              { name: 'Salário', icon: 'travel', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'tranport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ]
+          }
+        }
+      }
+    });
+
+    return { name: user.name, email: user.email };
+  }
+}
